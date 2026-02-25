7b9e2e1711dafb88557359e201f1ca177863dec7
Doru Blânzeanu <dblnz@pm.me>
2026-02-25 12:26:12 +0200

    Add copyright 2026 headers for source code
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight-js/src/lib.rs
+++ src/hyperlight-js/src/lib.rs
@@ -1,3 +1,18 @@
+/*
+Copyright 2026  The Hyperlight Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 //! This crate provides a Hyperlight implementation for JavaScript guest code.
 #![deny(dead_code, missing_docs, unused_mut)]
 #![cfg_attr(not(any(test, debug_assertions)), warn(clippy::panic))]
